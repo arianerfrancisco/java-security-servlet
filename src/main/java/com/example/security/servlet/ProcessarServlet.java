41e59eec5da137921ec88fa1214ff3fb14671b33
Ariane Rocha <72419533+arianerfrancisco@users.noreply.github.com>
2022-03-11 15:32:20 -0300

    update projeto

---
+++ src/main/java/com/example/security/servlet/ProcessarServlet.java
@@ -0,0 +1,18 @@
+package com.example.security.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ProcessarServlet", value = "/ProcessarServlet")
+public class ProcessarServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("Servlet Executado!");
+        response.getWriter().close();
+    }
+
+
+}
